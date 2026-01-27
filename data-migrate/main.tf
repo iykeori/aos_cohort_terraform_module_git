8bc501ebf68714bc107b930f2eb675a546235fac
iykeori <oricletus@gmail.com>
2026-01-27 17:26:37 -0600

    added modules changes

--- data-migrate/main.tf
+++ data-migrate/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "data_migrate_ec2" {
   ami                    = var.amazon_linux_ami_id
   instance_type          = var.ec2_instance_type
   subnet_id              = var.private_app_subnet_az1_id
-  vpc_security_group_ids = [db_migrate_server_security_group_id]
+  vpc_security_group_ids = [var.db_migrate_server_security_group_id]
   iam_instance_profile   = var.ec2_instance_profile_role_name
 
   user_data_base64 = base64encode(templatefile("${path.module}/db-migrate-script.sh.tpl", {
