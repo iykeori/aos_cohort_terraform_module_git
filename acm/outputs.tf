a6db200069aa2fe0836f0045fca5fec46476acf9
iykeori <oricletus@gmail.com>
2026-01-27 17:38:36 -0600

    added modules changes

--- acm/outputs.tf
+++ acm/outputs.tf
@@ -1,7 +1,8 @@
 # ACM 
 output "certificate_arn" {
   description = "The ARN of the ACM certificate"
-  value       = aws_acm_certificate.acm_certificate.arn
+  value       = aws_acm_certificate_validation.acm_certificate_validation.certificate_arn
+
 }
 
 output "domain_name" {
