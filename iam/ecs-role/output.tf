e62739784cece4d4aabb0929f0d7f1f37374a8a3
iykeori <oricletus@gmail.com>
2026-02-06 22:23:27 -0600

    added Ecs update

--- iam/ecs-role/output.tf
+++ iam/ecs-role/output.tf
@@ -1,5 +1,5 @@
 output "ecs_task_execution_role_arn" {
-  value = aws_iam_role.ecs_tasks_execution_role.arn
+  value = aws_iam_role.ecs_task_execution_role.arn
 }
 # ECS Task Role 
 output "ecs_task_role_arn" {
